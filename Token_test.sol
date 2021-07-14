442e3dd4ce0b7816758936179cbb035ceda94eda
Worden Tay <86772030+Acroburst@users.noreply.github.com>
2021-07-14 20:51:47 +0800

    Update Token_test.sol

--- Token_test.sol
+++ Token_test.sol
@@ -27,15 +27,6 @@ contract testSuite {
         Assert.equal(tkn.totalSupply(), 10000000000000000000000, 'token balance should be 10000 * 10 ** 18 initially');
     }
 
-    /// Custom Transaction Context: https://remix-ide.readthedocs.io/en/latest/unittesting.html#customization
-    /// #sender: account-1
-    /// #value: 100
-    function checkSenderAndValue() public payable {
-        // account index varies 0-9, value is in wei
-        Assert.equal(msg.sender, TestsAccounts.getAccount(1), "Invalid sender");
-        Assert.equal(msg.value, 100, "Invalid value");
-    }
-}
 
 contract testTransfer is Token {
    
